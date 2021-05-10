36705d877fb08ae58bc28b21e5f8c77264e375c9
giliover <giliover@hotmail.com>
2021-05-10 14:39:53 -0300

    Added: App component

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CommentBox from "./CommentBox";
+import CommentList from "./CommentList";
+
+const App = () => {
+	return (
+		<div>
+			<CommentBox />
+			<CommentList />
+		</div>
+	);
+};
+
+export default App;
