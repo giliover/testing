360b62a6aa737e689b4e1a25b2ca45ebac5fa691
giliover <giliover@hotmail.com>
2021-05-20 08:40:07 -0300

    Chore: implementing the CommentBox

--- client/src/components/CommentBox.js
+++ client/src/components/CommentBox.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const CommentBox = () => {
-    return <div>Comment Box</div>
+class CommentBox extends Component {
+	constructor() {
+		super()
+		this.state = {
+			comment: ``,
+		}
+	}
+
+	handleChange(event) {
+		setState({ comment: event.target.value })
+	}
+
+	handleSubmit(event) {
+		event.preventDefault()
+
+		setState({ comment: '' })
+	}
+
+	render() {
+		return (
+			<form>
+				<h4>Add a Comment</h4>
+				<textarea
+					onChange={this.handleChange}
+					value={this.state.comment}
+				/>
+				<button>Submit</button>
+			</form>
+		)
+	}
 }
 
 export default CommentBox
